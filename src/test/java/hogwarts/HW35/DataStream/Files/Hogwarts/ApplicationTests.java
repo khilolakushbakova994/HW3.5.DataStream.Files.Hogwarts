2dc5a1627160baf95c4bc1e7a96634011c74f1a3
khilola.kushbakova <xilola_kushbakova@mai.ru>
2024-01-13 17:22:53 +0100

    Initial commit

---
+++ src/test/java/hogwarts/HW35/DataStream/Files/Hogwarts/ApplicationTests.java
@@ -0,0 +1,13 @@
+package hogwarts.HW35.DataStream.Files.Hogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
